c0f313cf5bf3921ce12b9afac6a1a227142eff01
khechinimed <khechinibakr20@gmail.com>
2023-06-02 01:38:59 +0200

    Firebase Configuration

--- TODO-LIST/AppDelegate.swift
+++ TODO-LIST/AppDelegate.swift
@@ -6,15 +6,17 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      FirebaseApp.configure()
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+      return true
     }
 
     // MARK: UISceneSession Lifecycle
@@ -31,6 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
+
+
 
---
+++ TODO-LIST/SigninViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SigninViewModel.swift
+//  TODO-LIST
+//
+//  Created by Mohamed Khechini on 02/06/2023.
+//
+
+import Foundation
