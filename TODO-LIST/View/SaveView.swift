a8622f9094e2d8dc088bcb4e122430a76d6325c8
khechinimed <khechinibakr20@gmail.com>
2023-06-06 12:24:39 +0200

    SaveTask View

--- TODO-LIST/View/SaveView.swift
+++ TODO-LIST/View/SaveView.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct SaveView: View {
+    @Binding var Sheet:Bool
+    @Binding var textfiled: String
+    @EnvironmentObject var signinViewmodel : SigninViewModel
+    var body: some View{
+        VStack{
+            HStack{
+                Text("Title")
+                    .font(.headline)
+                Spacer()
+                Button(action: {
+                    Sheet.toggle()
+                }, label: {
+                    Image(systemName: "xmark")
+                        .font(.title2)
+                        .foregroundColor(.black)
+                })
+            }
+            TextField("type title", text: $textfiled)
+                .padding()
+                .frame(width: 290, height: 45)
+                .background(.ultraThinMaterial.opacity(0.9))
+            Button(action: {
+                if !textfiled.isEmpty{
+                    signinViewmodel.SaveTask(title: textfiled, done:signinViewmodel.done)
+                    textfiled = ""
+                    Sheet = false
+                }
+            }, label: {
+                Text("Save").bold()
+                    .foregroundColor(.black)
+            })
+        }
+    }
+}
+
+struct SaveView_Previews: PreviewProvider{
+    static var previews: some View {
+        SaveView(Sheet: .constant(true), textfiled: .constant(""))
+            .environmentObject(SigninViewModel())
+    }
+}
